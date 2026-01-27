f89dc86bfb9a7bfb903f85c293b65ae2ee788b71
IDGS-801-23001550 <vmnoe2520@gmail.com>
2026-01-26 18:57:21 -0600

    macros

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app =  Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    titulo = "Flask IDGS801"
+    lista = ["Juan", "Mario", "Pedro", "Dario"]
+    return render_template("index.html", titulo=titulo, lista=lista)
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
+
+@app.route("/operasBas", methods=['GET', 'POST'])
+def operas1():
+    n1 = 0
+    n2 = 0
+    res = 0
+
+    if request.method == 'POST':
+        n1 = request.form.get('n1')
+        n2 = request.form.get('n2')
+        res = float(n1) + float(n2)
+    return render_template("operasBas.html", n1=n1, n2=n2, res=res)
+
+@app.route("/resultado", methods=['GET', 'POST'])
+def resultado():
+    n1 = request.form.get('n1')
+    n2 = request.form.get('n2')
+
+    tem = float(n1)+float(n2)
+
+    return f"La suma es: {tem}"
 
 @app.route("/hola")
 def hola():
@@ -18,12 +49,12 @@ def user(user):
 def numero(n):
     return f"<h1>El numero es: {n}<h1>"
 
-@app.route("/user/<int>:id/<string:user>")
+@app.route("/user/<int:id>/<string:user>")
 def username(id, username):
     return f"<h1>¡Hola, {username}! tú ID es: {id}<h1>"
 
-@app.route("/suma/<float>:n1/<float:n2>")
-def username(n1, n2):
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1, n2):
     return f"<h1>La suma es: {n1+n1}<h1>"
 
 @app.route("/default/")
@@ -43,5 +74,5 @@ def operas():
     </form>
             '''
 
-if __name__ == "__Main__":
+if __name__ == "__main__":
     app.run(debug=True)
